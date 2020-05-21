bcc4f3cb1c8c52957d3d84bf50cab124f0106abf
Chandra Challagonda <challago@gmail.com>
2020-05-22 00:02:58 +0300

    Update entrypoint.sh
    Changed # to |

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,26 +55,26 @@ if [[ -n "$KEYROCK" ]]; then
 #	change_value "SPAGOBI.SECURITY.USER-PROFILE-FACTORY-CLASS.className" "it.eng.spagobi.security.OAuth2SecurityServiceSupplier"
 #	change_value "SPAGOBI_SSO.SECURITY_LOGOUT_URL" "${LOGOUT_URL}"
 
-	sed -i "s#it.eng.spagobi.services.common.JWTSsoService#it.eng.spagobi.services.oauth2.Oauth2SsoService#g" $SERVER_CONF
-	sed -i "s#<!-- START OAUTH 2#<!-- START OAUTH 2 -->#g" $WEB_XML && sed -i "s#END OAUTH 2 -->#<!-- END OAUTH 2 -->#g" $WEB_XML
-	sed -i "s#http://192.168.28.183:8080#${SERVER_NAME}#g" $WEB_XML
-	sed -i "s#it.eng.spagobi.commons.initializers.metadata.MetadataInitializer#it.eng.spagobi.commons.initializers.metadata.OAuth2MetadataInitializer#g" $INITIALIZER_XML
+	sed -i "s|it.eng.spagobi.services.common.JWTSsoService|it.eng.spagobi.services.oauth2.Oauth2SsoService|g" $SERVER_CONF
+	sed -i "s|<!-- START OAUTH 2|<!-- START OAUTH 2 -->|g" $WEB_XML && sed -i "s|END OAUTH 2 -->|<!-- END OAUTH 2 -->|g" $WEB_XML
+	sed -i "s|http://192.168.28.183:8080|${SERVER_NAME}|g" $WEB_XML
+	sed -i "s|it.eng.spagobi.commons.initializers.metadata.MetadataInitializer|it.eng.spagobi.commons.initializers.metadata.OAuth2MetadataInitializer|g" $INITIALIZER_XML
 
 	unzip $KNOWAGE_JAR -d $UNZIPPED_JAR
-	sed -i "s#CLIENT_ID.*#CLIENT_ID=${KEYROCK_CLIENT_ID}#g" $KNOWAGE_CONF
-	sed -i "s#SECRET.*#SECRET=${KEYROCK_SECRET}#g" $KNOWAGE_CONF
-	sed -i "s#AUTHORIZE_URL.*#AUTHORIZE_URL=${KEYROCK_AUTH_URL}#g" $KNOWAGE_CONF
-	sed -i "s#ACCESS_TOKEN_URL.*#ACCESS_TOKEN_URL=${KEYROCK_TOKEN_URL}#g" $KNOWAGE_CONF
-	sed -i "s#USER_INFO_URL.*#USER_INFO_URL=${KEYROCK_USER_URL}#g" $KNOWAGE_CONF
-	sed -i "s#REDIRECT_URI.*#REDIRECT_URI=${KEYROCK_REDIRECT_URI}#g" $KNOWAGE_CONF
-	sed -i "s#REST_BASE_URL.*#REST_BASE_URL=${KEYROCK_REST_URL}#g" $KNOWAGE_CONF
-	sed -i "s#TOKEN_PATH.*#TOKEN_PATH=${KEYROCK_TOKEN_PATH}#g" $KNOWAGE_CONF
-	sed -i "s#ROLES_PATH.*#ROLES_PATH=${KEYROCK_ROLES_PATH}#g" $KNOWAGE_CONF
-	sed -i "s#ORGANIZATION_INFO_PATH.*#ORGANIZATION_INFO_PATH=${KEYROCK_ORG_INFO_PATH}#g" $KNOWAGE_CONF
-	sed -i "s#APPLICATION_ID.*#APPLICATION_ID=${KEYROCK_APPLICATION_ID}#g" $KNOWAGE_CONF
-	sed -i "s#ADMIN_ID.*#ADMIN_ID=${KEYROCK_ADMIN_ID}#g" $KNOWAGE_CONF
-	sed -i "s#ADMIN_EMAIL.*#ADMIN_EMAIL=${KEYROCK_ADMIN_EMAIL}#g" $KNOWAGE_CONF
-	sed -i "s#ADMIN_PASSWORD.*#ADMIN_PASSWORD=${KEYROCK_ADMIN_PASSWORD}#g" $KNOWAGE_CONF
+	sed -i "s|CLIENT_ID.*|CLIENT_ID=${KEYROCK_CLIENT_ID}|g" $KNOWAGE_CONF
+	sed -i "s|SECRET.*|SECRET=${KEYROCK_SECRET}|g" $KNOWAGE_CONF
+	sed -i "s|AUTHORIZE_URL.*|AUTHORIZE_URL=${KEYROCK_AUTH_URL}|g" $KNOWAGE_CONF
+	sed -i "s|ACCESS_TOKEN_URL.*|ACCESS_TOKEN_URL=${KEYROCK_TOKEN_URL}|g" $KNOWAGE_CONF
+	sed -i "s|USER_INFO_URL.*|USER_INFO_URL=${KEYROCK_USER_URL}|g" $KNOWAGE_CONF
+	sed -i "s|REDIRECT_URI.*|REDIRECT_URI=${KEYROCK_REDIRECT_URI}|g" $KNOWAGE_CONF
+	sed -i "s|REST_BASE_URL.*|REST_BASE_URL=${KEYROCK_REST_URL}|g" $KNOWAGE_CONF
+	sed -i "s|TOKEN_PATH.*|TOKEN_PATH=${KEYROCK_TOKEN_PATH}|g" $KNOWAGE_CONF
+	sed -i "s|ROLES_PATH.*|ROLES_PATH=${KEYROCK_ROLES_PATH}|g" $KNOWAGE_CONF
+	sed -i "s|ORGANIZATION_INFO_PATH.*|ORGANIZATION_INFO_PATH=${KEYROCK_ORG_INFO_PATH}|g" $KNOWAGE_CONF
+	sed -i "s|APPLICATION_ID.*|APPLICATION_ID=${KEYROCK_APPLICATION_ID}|g" $KNOWAGE_CONF
+	sed -i "s|ADMIN_ID.*|ADMIN_ID=${KEYROCK_ADMIN_ID}|g" $KNOWAGE_CONF
+	sed -i "s|ADMIN_EMAIL.*|ADMIN_EMAIL=${KEYROCK_ADMIN_EMAIL}|g" $KNOWAGE_CONF
+	sed -i "s|ADMIN_PASSWORD.*|ADMIN_PASSWORD=${KEYROCK_ADMIN_PASSWORD}|g" $KNOWAGE_CONF
 	sed -i '${s/$/\nSTATE=true/}' $KNOWAGE_CONF
 	cd $UNZIPPED_JAR; zip -r $KNOWAGE_JAR *
 	rm -rf $UNZIPPED_JAR
